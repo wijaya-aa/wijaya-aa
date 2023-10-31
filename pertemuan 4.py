6ecef36f209302a3dbb2a09e42e34f5b40887a81
wijaya88 <148929822+wijaya-aa@users.noreply.github.com>
2023-10-31 08:47:27 +0700

    Add files via upload

---
+++ pertemuan 4.py
@@ -0,0 +1,9 @@
+print("IDENTITAS DIRI")
+print("***************")
+nama= input("masukkan nama anda")
+npm= input("masukkan npm")
+prodi=input("masukkan prodi")
+fakultas=input("fakultas")
+
+
+print("Halo, perkenalkan nama saya{} saya adalah mahasiswa baru di prodi{} fakultas{}".format(nama,prodi,fakultas))
